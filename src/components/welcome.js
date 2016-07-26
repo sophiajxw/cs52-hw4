28e6463ccdc0734d84628ec1277d86e24d64bbfc
Tim Tregubov <tim@zingweb.com>
2016-07-25 23:14:22 -0400

    with redux

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
+import Counter from '../containers/counter';
+import Controls from '../containers/controls';
+
 
 // function based "dumb" component with no state
 const Welcome = () => {
   return (
     <div>
-      react+react-router+webpack+babel+eslint starter
+      react+redux+react-router+webpack+babel+eslint starter
+
+      <Counter />
+      <Controls />
     </div>
   );
 };
