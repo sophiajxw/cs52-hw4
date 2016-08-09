3072505cee68256648f73b4cb442a793b3ab47f7
sophiajiang <sophiajxw@gmail.com>
2016-08-08 21:17:40 -0400

    deploy to my server

--- src/actions/index.js
+++ src/actions/index.js
@@ -11,7 +11,9 @@ export const ActionTypes = {
 };
 
 // const ROOT_URL = 'https://cs52-blog.herokuapp.com/api';
-const ROOT_URL = 'http://localhost:9090/api';
+// const ROOT_URL = 'http://localhost:9090/api';
+const ROOT_URL = 'https://cs52-hw5.herokuapp.com/api';
+
 
 const API_KEY = '?key=xinwei_jiang';
 
