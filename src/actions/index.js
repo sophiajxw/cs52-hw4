44fa4cfd797fb4075a3be6c4cf39c51b260f935a
sophiajiang <sophiajxw@gmail.com>
2016-08-14 13:12:09 -0400

    working

--- src/actions/index.js
+++ src/actions/index.js
@@ -14,8 +14,8 @@ export const ActionTypes = {
 };
 
 // const ROOT_URL = 'https://cs52-blog.herokuapp.com/api';
-const ROOT_URL = 'http://localhost:9090/api';
-// const ROOT_URL = 'https://hw5p2.herokuapp.com/api';
+// const ROOT_URL = 'http://localhost:9090/api';
+const ROOT_URL = 'https://hw5p2.herokuapp.com/api';
 
 
 const API_KEY = '?key=xinwei_jiang';
